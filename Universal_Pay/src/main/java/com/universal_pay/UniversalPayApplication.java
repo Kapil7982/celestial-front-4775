0a50c24d9d7e9c548801427eded1ac1fcb4f5310
Kapil7982 <rathor798299@gmail.com>
2022-11-08 22:24:16 +0530

    Create repo

---
+++ Universal_Pay/src/main/java/com/universal_pay/UniversalPayApplication.java
@@ -0,0 +1,13 @@
+package com.universal_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalPayApplication.class, args);
+	}
+
+}
---
+++ Universal_Pay/src/test/java/com/universal_pay/UniversalPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universal_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
