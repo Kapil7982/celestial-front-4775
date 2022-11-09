bf35378946c0a617f02ba6b609553c31e2f57f1d
Ashirvad121 <ashirvadkumar.121@gmail.com>
2022-11-10 00:13:23 +0530

    added IBenificaryserviceImpl

--- Universal_Pay/src/main/java/com/universal_pay/service/IBenificiaryServiceImpl.java
+++ Universal_Pay/src/main/java/com/universal_pay/service/IBenificiaryServiceImpl.java
@@ -1,5 +1,34 @@
 package com.universal_pay.service;
 
-public class IBenificiaryServiceImpl {
+import java.util.List;
+
+import com.universal_pay.model.BeneficiaryDetails;
+import com.universal_pay.model.Customer;
+
+public class IBenificiaryServiceImpl implements IBenificiaryService{
+
+	@Override
+	public BeneficiaryDetails addBenificiary(BeneficiaryDetails bd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BeneficiaryDetails deleteBenificiary(BeneficiaryDetails bd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BeneficiaryDetails viewBenificiary(String mobNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BeneficiaryDetails> viewAllBeneficiary(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
