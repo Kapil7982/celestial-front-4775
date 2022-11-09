ba668a871e1d85c5ee6765e0c2d50ec31b9bd020
Kapil7982 <rathor798299@gmail.com>
2022-11-09 15:31:43 +0530

    All dependency done

--- Universal_Pay/src/main/java/com/universal_pay/UniversalPayApplication.java
+++ Universal_Pay/src/main/java/com/universal_pay/UniversalPayApplication.java
@@ -3,7 +3,10 @@ package com.universal_pay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class UniversalPayApplication {
 
 	public static void main(String[] args) {
