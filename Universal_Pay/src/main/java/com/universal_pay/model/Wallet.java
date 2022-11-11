2d0c104b9ccb71aeca9bebbd70a7be81b7a368e4
Kapil7982 <rathor798299@gmail.com>
2022-11-11 22:52:56 +0530

    All set

--- Universal_Pay/src/main/java/com/universal_pay/model/Wallet.java
+++ Universal_Pay/src/main/java/com/universal_pay/model/Wallet.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,6 +16,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -23,60 +26,87 @@ public class Wallet {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int walledId;
-	private BigDecimal balance;
+	private int walletId;
+	private Double balance;
 	
-	@OneToOne(cascade = CascadeType.ALL, mappedBy = "wallet")
-	private Customer customer;
+	@OneToMany(mappedBy = "wallet" , fetch = FetchType.EAGER)
+	@Fetch(value = FetchMode.SUBSELECT)
+	private List<BankAccount> bankAccounts;
 	
-//	@Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
-	@OneToMany(cascade = CascadeType.MERGE, mappedBy = "wallet")
-	@JsonIgnore
-	private Set<BankAccount> bankAccounts = new HashSet<>();
+	@OneToMany(mappedBy = "wallet" , fetch = FetchType.EAGER)
+	@Fetch(value = FetchMode.SUBSELECT)
+	private List<Transaction> transcations;
 	
+	@OneToMany(mappedBy = "wallet" , fetch = FetchType.EAGER)
+	@Fetch(value = FetchMode.SUBSELECT)
+	private List<BillPayment> billPayments;
+	
+	@OneToMany(mappedBy = "wallet" , fetch = FetchType.EAGER)
+	@Fetch(value = FetchMode.SUBSELECT)
+	private List<BeneficiaryDetails> benificiaryDetails;
+
 	public Wallet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public Wallet(BigDecimal amount) {
-		this.balance=amount;
+	public Wallet(int walletId, Double balance, List<BankAccount> bankAccounts, List<Transaction> transcations,
+			List<BillPayment> billPayments, List<BeneficiaryDetails> benificiaryDetails) {
+		super();
+		this.walletId = walletId;
+		this.balance = balance;
+		this.bankAccounts = bankAccounts;
+		this.transcations = transcations;
+		this.billPayments = billPayments;
+		this.benificiaryDetails = benificiaryDetails;
 	}
 
-	public int getWalledId() {
-		return walledId;
+	public int getWalletId() {
+		return walletId;
 	}
 
-	public void setWalledId(int walledId) {
-		this.walledId = walledId;
+	public void setWalletId(int walletId) {
+		this.walletId = walletId;
 	}
 
-	public BigDecimal getBalance() {
+	public Double getBalance() {
 		return balance;
 	}
 
-	public void setBalance(BigDecimal balance) {
+	public void setBalance(Double balance) {
 		this.balance = balance;
 	}
-	
-	
 
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
-
-	public Set<BankAccount> getBankAccounts() {
+	public List<BankAccount> getBankAccounts() {
 		return bankAccounts;
 	}
 
-	public void setBankAccounts(Set<BankAccount> bankAccounts) {
+	public void setBankAccounts(List<BankAccount> bankAccounts) {
 		this.bankAccounts = bankAccounts;
 	}
 
+	public List<Transaction> getTranscations() {
+		return transcations;
+	}
+
+	public void setTranscations(List<Transaction> transcations) {
+		this.transcations = transcations;
+	}
+
+	public List<BillPayment> getBillPayments() {
+		return billPayments;
+	}
+
+	public void setBillPayments(List<BillPayment> billPayments) {
+		this.billPayments = billPayments;
+	}
+
+	public List<BeneficiaryDetails> getBenificiaryDetails() {
+		return benificiaryDetails;
+	}
+
+	public void setBenificiaryDetails(List<BeneficiaryDetails> benificiaryDetails) {
+		this.benificiaryDetails = benificiaryDetails;
+	}
 	
 	
 	
