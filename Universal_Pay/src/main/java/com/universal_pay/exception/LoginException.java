506e0c0c67341511f88ca72bcaf5d8099f11b8f4
amarsarkar007 <2016sarkaramar@gmail.com>
2022-11-12 18:53:21 +0530

    added login module

---
+++ Universal_Pay/src/main/java/com/universal_pay/exception/LoginException.java
@@ -0,0 +1,13 @@
+package com.universal_pay.exception;
+
+public class LoginException extends Exception{
+	
+	public LoginException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public LoginException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ Universal_Pay/src/main/java/com/universal_pay/model/LoginDTO.java
@@ -0,0 +1,23 @@
+package com.universal_pay.model;
+
+public class LoginDTO {
+	
+	private String mobileNumber;
+	private String password;
+	
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
