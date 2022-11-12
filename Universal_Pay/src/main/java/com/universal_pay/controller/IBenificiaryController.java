d743f86ba8a93d1530f2690c1198a316976463a4
Ashirvad121 <ashirvadkumar.121@gmail.com>
2022-11-12 15:07:21 +0530

    updated IBenificiaryController

--- Universal_Pay/src/main/java/com/universal_pay/controller/IBenificiaryController.java
+++ Universal_Pay/src/main/java/com/universal_pay/controller/IBenificiaryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.universal_pay.exception.BenificiaryException;
@@ -19,6 +20,7 @@ import java.util.List;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
+@RequestMapping("/pwa-payment")
 public class IBenificiaryController {
 	@Autowired
 	private IBenificiaryService ibservice;
